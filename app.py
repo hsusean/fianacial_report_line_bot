73446a24887711d5770633cee633f2c15127f291
hsusean <hsusean1219@gmail.com>
2022-08-22 00:20:36 +0800

    [update]finance_report_crawler

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def handle_message(event):
     report_type = get_message[0]
     stock_id = get_message[1:]
     res = get_each_stock_finance_report(stock_id, report_type)
+    print(11111, res)
     # Send To Line
-    reply = TextSendMessage(text='''{}'''.format(123))
+    reply = TextSendMessage(text='''{}'''.format(res))
     line_bot_api.reply_message(event.reply_token, reply)
