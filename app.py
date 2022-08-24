ff8121d883ba1e080ad083e2dc190e0ce1523d05
hsusean <hsusean1219@gmail.com>
2022-08-24 23:53:00 +0800

    [update]apt

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_message(event):
     report_type = get_message[0]
     stock_id = get_message[1:]
     res = get_each_stock_finance_report(stock_id, report_type)
-    print(11111, res)
+    print(111111, res)
     # Send To Line
     # reply = TextSendMessage(text='''{}'''.format(res))
     reply = ImageSendMessage(
