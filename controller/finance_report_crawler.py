85dbf422643e38b4ea2b5b04b811b108e6d26f1c
hsusean <hsusean1219@gmail.com>
2022-08-31 23:43:01 +0800

    [update]create_chrome

--- controller/finance_report_crawler.py
+++ controller/finance_report_crawler.py
@@ -102,7 +102,7 @@ def get_each_stock_finance_report(stock_id, report_type, year=None, season=None,
     df.columns = df_cols
     df = df.set_index('item')
     print(11111, df)
-    print(11112, os.getcwd())
+    print(11112, os.getcwd()) 
 
     # TaipeiSansTCBeta-Regular
     dfi.export(df, '123.png',table_conversion='chrome')
